fcc6f6e781ffd014ff49b26e99355717a4c1b248
Mike <mikenock24@gmail.com>
2021-03-01 00:43:29 -0500

    wip

---
+++ WebFundamentals/Javascript/basic_foundation/index.js
@@ -0,0 +1,25 @@
+console.log('Number 1 - Get 1 to 255')
+
+console.log('Number 2 - Get even 1000')
+
+console.log('Number 3 - Sum odd 5000')
+
+console.log('Number 4 - Iterate a array')
+
+console.log('Number 5 - Find Max')
+
+console.log('Number 6 - Find Average')
+
+console.log('Number 7 - Array Odd')
+
+console.log('Number 8 - Greater than Y')
+
+console.log('Number 9 - Squares')
+
+console.log('Number 10 - Negatives')
+
+console.log('Number 11 - Max/Min/Avg')
+
+console.log('Number 12 - Swap Values')
+
+console.log('Number 13 - Number to String')
