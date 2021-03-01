cc185b193f10d041a8caa62637820a0313a388ec
Mike <mikenock24@gmail.com>
2021-03-01 01:44:22 -0500

    wip

---
+++ WebFundamentals/Javascript/playing_with_objects/user_select.js
@@ -0,0 +1,13 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+// Number 1 - Print/log Johns Age
+console.log(users[1].age)
+
+// Number 2 - Print/log The name of the first object
+console.log(users[0].name)
+
+// Number 3 - Print/log the name and age of each user using a for loop
+console.log(users)
+// for(i=0;i>users.length;i++){
+//     console.log(users[i].name + '-' + user[i].age)
+// }
