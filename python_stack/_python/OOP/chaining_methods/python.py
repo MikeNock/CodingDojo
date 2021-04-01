20dc9246986dd481836868c60c058eec8b52a8c3
Mike <mikenock24@gmail.com>
2021-04-01 18:39:27 -0400

    chaining_methods

--- python_stack/_python/OOP/chaining_methods/python.py
+++ python_stack/_python/OOP/chaining_methods/python.py
@@ -6,30 +6,21 @@ class User:
         self.account_balance = 0
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
-        self.account_balance = amount
+        print(self.account_balance)
     def make_transfer(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 Mike = User("Mike", "mike@nockops.com")
 Kaesy = User("Kaesy", "kaesy@nockops.com")
 Ros = User("Ros", "roseller.yu@nockops.com")
-Mike.make_deposit(10000)
-Mike.make_deposit(100000)
-Mike.make_deposit(1000000)
-Mike.make_withdrawal(1000)
-print(Mike.account_balance)
-Kaesy.make_deposit(100000)
-Kaesy.make_deposit(1000000)
-Kaesy.make_withdrawal(1000)
-Kaesy.make_withdrawal(500)
-print(Kaesy.account_balance)
-Ros.make_deposit(10000)
-Ros.make_withdrawal(1000)
-Ros.make_withdrawal(800)
-Ros.make_withdrawal(600)
-Mike.make_transfer(Ros, 10000)
-print(Mike.account_balance)
-print(Ros.account_balance)
+Ros.make_deposit(10000).make_withdrawal(1000).make_withdrawal(800).make_withdrawal(600)
+Mike.make_deposit(10000).make_deposit(100000).make_deposit(1000000).make_withdrawal(1000).display_user_balance()
+Kaesy.make_deposit(100000).make_deposit(1000000).make_withdrawal(1000).make_withdrawal(500).display_user_balance()
+Mike.make_transfer(Ros, 10000).display_user_balance()
+Ros.display_user_balance()
