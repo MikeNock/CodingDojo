6c5df967ebeeded5fec6e3bbf819c384a92e8e85
Mike <mikenock24@gmail.com>
2021-06-20 19:37:37 -0400

    random_word

--- python_stack/django/django_intro/random_generator/random_generator/urls.py
+++ python_stack/django/django_intro/random_generator/random_generator/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('random_word.urls')),
-    path('random_word', include('random_word.urls'))
+    path('random_word', include('random_word.urls')),
+    # path('random_word', include('random_word.urls'))
 ]
--- python_stack/django/django_intro/random_generator/random_word/urls.py
+++ python_stack/django/django_intro/random_generator/random_word/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.random_word),
-    path('random_word', views.random_word)
+    # path('', views.index),
+    # path('', views.index),
+    path('random_word', views.random_word),
+    path('reset', views.reset),
 ]
