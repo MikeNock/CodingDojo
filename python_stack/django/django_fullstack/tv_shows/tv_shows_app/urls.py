edc285a724ee74e5dd2a705b27d3241005edd56c
Mike <mikenock24@gmail.com>
2021-07-01 02:13:48 -0400

    wip

--- python_stack/django/django_fullstack/tv_shows/tv_shows_app/urls.py
+++ python_stack/django/django_fullstack/tv_shows/tv_shows_app/urls.py
@@ -2,12 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # localhost:8000/shows
     path('', views.index),
-    path('shows', views.index),
-    path('shows/new', views.index),
-    path('shows/create', views.index),
-    path('shows/<id>', views.index),
-    path('shows/<id>/edit', views.index),
-    path('shows/<id>/update', views.index),
-    path('shows/<id>/destroy', views.index),
+    # localhost:8000/shows/new
+    path('new', views.new),
+    # localhost:8000/shows/create
+    path('create', views.create),
+    # localhost:8000/shows/<show_id>/edit
+    path('<int:show_id>/edit', views.edit),
+    # localhost:8000/shows/<show_id>/update
+    path('<int:show_id>/update', views.update),
+    # localhost:8000/shows/<show_id>
+    path('<int:show_id>', views.show),
+    # localhost:8000/shows/<show_id>/delete
+    path('<int:show_id>/delete', views.delete)
 ]
